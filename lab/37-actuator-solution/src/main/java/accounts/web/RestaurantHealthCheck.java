e41a5f4c4d26b87db6b4953cbb91ad7a7bdec716
Benjamin Smith <benjamin.lee.smith@gmail.com>
2018-08-02 09:19:39 -0600

    added custom health check

---
+++ lab/37-actuator-solution/src/main/java/accounts/web/RestaurantHealthCheck.java
@@ -0,0 +1,27 @@
+package accounts.web;
+
+import accounts.AccountManager;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+import rewards.internal.account.Account;
+import rewards.internal.restaurant.RestaurantRepository;
+
+@Component
+public class RestaurantHealthCheck implements HealthIndicator {
+    private final RestaurantRepository restaurantRepository;
+
+    public RestaurantHealthCheck(RestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    @Override
+    public Health health() {
+        if(restaurantRepository.getRestaurantCount() > 1) {
+            return Health.up().build();
+        }
+        else {
+            return Health.down().build();
+        }
+    }
+}
