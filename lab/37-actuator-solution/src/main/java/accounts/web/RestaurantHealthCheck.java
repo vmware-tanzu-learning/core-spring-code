17899d5fef8fbc32e5b5a74fc1dbe2e1bf814f07
Benjamin Smith <benjamin.lee.smith@gmail.com>
2018-08-02 15:35:23 -0600

    Clean up imports

--- lab/37-actuator-solution/src/main/java/accounts/web/RestaurantHealthCheck.java
+++ lab/37-actuator-solution/src/main/java/accounts/web/RestaurantHealthCheck.java
@@ -1,11 +1,8 @@
 package accounts.web;
 
-import accounts.AccountManager;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
-import rewards.internal.account.Account;
-import rewards.internal.restaurant.JpaRestaurantRepository;
 import rewards.internal.restaurant.RestaurantRepository;
 
 @Component
