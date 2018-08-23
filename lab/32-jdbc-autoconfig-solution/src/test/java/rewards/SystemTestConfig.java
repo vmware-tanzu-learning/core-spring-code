8cd8b5bfee441224b57b3529c62628104215cfeb
Paul Chapman <chapman@zemsys.com>
2018-08-24 02:29:01 +0900

    Fix broken test

---
+++ lab/32-jdbc-autoconfig-solution/src/test/java/rewards/SystemTestConfig.java
@@ -0,0 +1,37 @@
+package rewards;
+
+import config.RewardsConfig;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import javax.sql.DataSource;
+
+/**
+ * Sets up an embedded in-memory HSQL database, primarily for testing.
+ */
+@Configuration
+@Import(RewardsConfig.class)
+public class SystemTestConfig {
+	private final Logger logger = LoggerFactory.getLogger(SystemTestConfig.class);
+
+	/**
+	 * Creates an in-memory "rewards" database populated 
+	 * with test data for fast testing
+	 */
+	@Bean
+	public DataSource dataSource() {
+		logger.debug("Creating the datasource bean explicitly");
+
+		return
+			(new EmbeddedDatabaseBuilder())
+			.addScript("classpath:rewards/testdb/schema.sql")
+			.addScript("classpath:rewards/testdb/data.sql")
+			.build();
+	}	
+	
+}
