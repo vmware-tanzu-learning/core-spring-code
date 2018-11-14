8ec7bb7267a43ff8e529254fdb273be2a8634005
sashinpivotal <sashin@pivotal.io>
2018-11-13 20:13:04 -0600

    annotation solution project uses rewards.internal as base scanning package

--- lab/16-annotations-solution/src/main/java/config/RewardsConfig.java
+++ lab/16-annotations-solution/src/main/java/config/RewardsConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("rewards")
+@ComponentScan("rewards.internal")
 public class RewardsConfig {
 
 }
