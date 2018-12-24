53342317fc9545f55ae10441112f9ae7d57fe981
sashinpivotal <sashin@pivotal.io>
2018-12-24 17:09:48 -0500

    a bit cleanup on actuator TODO

--- lab/42-actuator/src/test/java/accounts/web/RestaurantHealthCheckTest.java
+++ lab/42-actuator/src/test/java/accounts/web/RestaurantHealthCheckTest.java
@@ -1,9 +1,5 @@
 package accounts.web;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -11,16 +7,17 @@ import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.Status;
-
 import rewards.internal.restaurant.JpaRestaurantRepository;
 import rewards.internal.restaurant.RestaurantRepository;
+
+import static org.mockito.Mockito.*;
 
 /**
  * TODO-11: IMPORTANT: Stop the ActuatorApplication whilst we add new classes.
  *          <p>
  *          Modify this class to test the RestaurantHealthCheck class.
- *          This class will implement HealthCheck so a health()
- *          method will exist - use it in the tests.
+ *          The RestaurantHealthCheck class will implement HealthCheck
+ *          so a health() method will exist - use it in the tests.
  *          Code will not compile until the next step.
  */
 @RunWith(JUnitPlatform.class)
@@ -32,7 +29,7 @@ public class RestaurantHealthCheckTest {
 	public void setUp() {
 		restaurantRepository = mock(JpaRestaurantRepository.class);
 		
-		// TODO-12bc: Create an instance.
+		// TODO-12b: Create an instance of RestaurantHealthCheck class
 		// - Remove the two @Disabled annotations below
 		// - Run the test, make sure it passes.
 		restaurantHealthCheck = null;
