a86491f0237b5e2c139704c6432b3c202d3ad624
sashinpivotal <sashin@pivotal.io>
2019-03-14 15:32:57 -0400

    actuator lab - give some more hint on creating HealthIndicator bean

--- lab/42-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
+++ lab/42-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
@@ -2,12 +2,11 @@ package accounts.web;
 
 /**
  * TODO-15a: Make this class implement HealthIndicator
- * <p>
- * Add a constructor to pass in the restaurant repository and use it to
- * implement health().
- * <p>
- * health() should return DOWN if the repository is empty (no restaurants) or UP
- * otherwise.
+ *  - Make it a component
+ *  - Add a constructor to pass in the restaurant repository and use it to
+ *    implement health().
+ *  - health() should return DOWN if the repository is empty
+ *    (no restaurants) or UP otherwise.
  *
  * TODO-19 (Extra credit): Experiment with HealthIndicator (Read lab document)
  */
