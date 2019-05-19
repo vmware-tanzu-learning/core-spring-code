35c78fc56187d3b8b5c1f699a74c3957adf50658
sashinpivotal <sashin@pivotal.io>
2019-05-19 18:53:10 -0400

    12-javaconfig-dependency-injection - remove html tags from TODO

--- lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
+++ lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
@@ -2,31 +2,29 @@ package config;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 /**
  * TODO-01: This class will be used to define our application beans. Annotate to
  * mark it as a special class for providing Spring with bean configuration
  * instructions.
- * <p>
+ *
  * TODO-02: Define four empty @Bean methods, one for the reward-network and
  * three for the repositories.
  *  - The RewardNetworkImpl should the bean name 'rewardNetwork'.
  *  - We have provided three JDDC repository implementations for
  *    Accounts, Restaurants and Rewards. Their bean names should be
  *    `accountRepository`, `restaurantRepository` and `rewardRepository`.
- * <p>
+ *
  * TODO-03: Each repository has a DataSource property to set, but the DataSource
  * is defined elsewhere (TestInfrastructureConfig.java), so you will need to
  * define a constructor for this class that accepts a dataSource parameter.
  * As it is the only constructor, Spring will automatically call it, so
  * @Autowired is optional.
- * <p>
+ *
  * TODO-04: Implement each @Bean method to contain the code needed to
  * instantiate its object and set its dependencies. Be careful - do you need to
  * use a constructor or call a setter to set dependencies? If you aren't sure,
  * refer to the diagram in the lab-instructions for more details.
- * <p>
+ *
  * Each bean method should return an interface not an implementation type.
  */
 public class RewardsConfig {
--- lab/12-javaconfig-dependency-injection/src/test/java/rewards/TestInfrastructureConfig.java
+++ lab/12-javaconfig-dependency-injection/src/test/java/rewards/TestInfrastructureConfig.java
@@ -1,52 +1,50 @@
 package rewards;
 
-import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
-import rewards.internal.RewardNetworkImplTests;
+import javax.sql.DataSource;
 
 /**
  * TODO-06: Review this class - it creates an in-memory HSQL database and
  * populates it with data. If you are interested, the two scripts are in the
  * 'rewards-common' project in 'src/main/resources'.
- * <p>
+ *
  * DO NOT MODIFY THE BEAN METHOD.
- * <p>
+ *
  * TODO-07: Import your application configuration file (RewardsConfig)
- * <p>
+ *
  * TODO-08: Create a new JUnit 5 test called RewardNetworkTests
  * in this package (your IDE may call it JUnit Jupiter). Ask for
  * a setup() method to be generated.
- *<p>
+ *
  * NOTE: The appendices at the bottom of the course Home Page includes
  * a section on creating JUnit tests in an IDE.
- * <p>
+ *
  * TODO-09: There should be a setup() method already, if not add one and
  * annotate it with @BeforeEach.
  * In this method, you will need to create an application context using
  * this configuration class, then get the rewardNetwork bean and assign
  * it to a private field for use later.
- * <p>
+ *
  * TODO-10: We can test the setup by running an empty test.
- *  <ul>
- *  <li>If your IDE automatically generated a @Test method, rename it
+ *
+ *  - If your IDE automatically generated a @Test method, rename it
  *    testRewardForDining. Delete any code in the method body.
- *  <li>Otherwise add a testRewardForDining method & annotate it with
+ *  - Otherwise add a testRewardForDining method & annotate it with
  *    @Test (make sure to import org.junit.jupiter.api.Test).
- *  <li>Run the test. If your setup() is working you get a green bar.
- *  </ul>
+ *  - Run the test. If your setup() is working you get a green bar.
+ *
  * TODO-11: Finally run a real test.
- * <ul>
- *  <li>Copy the unit test (the @Test method) from
- *    {@link RewardNetworkImplTests#testRewardForDining()} - we are testing
+ *
+ *  - Copy the unit test (the @Test method) from
+ *    RewardNetworkImplTests#testRewardForDining() - we are testing
  *    the same code, but using a different setup.
- *  <li>Run the test - it should pass if you have configured everything
+ *  - Run the test - it should pass if you have configured everything
  *    correctly. Congratulations, you are done.
- *  <li>If your test fails - did you miss the import in TO DO 7 above?
- * </ul>
+ *  - If your test fails - did you miss the import in TO DO 7 above?
+ *
  */
 @Configuration
 public class TestInfrastructureConfig {
