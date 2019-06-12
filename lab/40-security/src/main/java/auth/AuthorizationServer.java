54222e23f2b4e30024e662ed19ad24810eb9822a
sashinpivotal <sashin@pivotal.io>
2019-06-12 19:20:47 -0400

    40-security lab - remove oauth lab constants

--- lab/40-security/src/main/java/auth/AuthorizationServer.java
+++ lab/40-security/src/main/java/auth/AuthorizationServer.java
@@ -20,15 +20,9 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 @SpringBootApplication(exclude = { JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 public class AuthorizationServer {
 
-	public static final String CLIENT_CREDENTIALS = "client_credentials";
-	public static final String ROLE_TRUSTED_CLIENT = "ROLE_TRUSTED_CLIENT";
-
-	public static final String ACCOUNT_READ = "account.read";
-	public static final String ACCOUNT_WRITE = "account.write";
-
 	public static void main(String[] args) {
 		// This is the name of the properties file to read. Instead of
-		// "application.properties" it will read "account-server.properties".
+		// "application.properties" it will read "auth-server.properties".
 		System.setProperty("spring.config.name", "auth-server");
 
 		SpringApplication.run(AuthorizationServer.class, args);
