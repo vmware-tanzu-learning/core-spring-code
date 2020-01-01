65b7522c621fb070618a6f9b459dc82551f6cc58
sashinpivotal <sashin@pivotal.io>
2020-01-01 18:38:16 -0500

    30-jdbc-boot-solution - user AssertJ

--- lab/30-jdbc-boot-solution/src/test/java/rewards/JdbcBootApplicationTests.java
+++ lab/30-jdbc-boot-solution/src/test/java/rewards/JdbcBootApplicationTests.java
@@ -5,20 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 public class JdbcBootApplicationTests {
     public static final String QUERY = "SELECT count(*) FROM T_ACCOUNT";
 
-    @Autowired JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     public void testNumberOfAccount() throws Exception {
 
         long count = jdbcTemplate.queryForObject(QUERY, Long.class);
 
-        assertEquals(21L, count);
+        assertThat(count).isEqualTo(21L);
     }
 
 }
