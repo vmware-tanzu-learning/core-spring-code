631cef5421ce083c3896c5ec7849ae51f4060012
sashinpivotal <sashin@pivotal.io>
2020-01-14 12:52:59 -0500

    security lab - change wordings in the TODO-02

--- lab/40-security/src/main/java/auth/AuthorizationServer.java
+++ lab/40-security/src/main/java/auth/AuthorizationServer.java
@@ -8,13 +8,14 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 /**
  * TODO-01: Run this application and open http://localhost:1111 in your browser.
  * You should see a login page.
- * The username is "user" and the password is in your console output.
+ * The username is "user" and the Spring Boot generated password is
+ * displayed in your console output.
  * (If the browser keeps displaying the login page, use Chrome Incognito browser.)
  *
- * TODO-02: Access "Recent requests" and "health" pages.
+ * TODO-02: Access "beans" and "health" actuator endpoints.
  *
  * TODO-03: What happens if you access the Superuser Only page?
- * You can access this page as a regular user.  This page is not protected.
+ * You can access this page as a regular user.  In other words, this page is not protected.
  * We will address that. Open the AuthServerConsoleSecurityConfiguration class.
  */
 @SpringBootApplication(exclude = { JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
