e31a2743b6fdfd81a23af8399d4d88e1cb84b686
sashinpivotal <sashin@pivotal.io>
2020-01-15 14:21:20 -0500

    80-security-views - clean up TODO wordings

--- lab/80-security-views/src/main/java/config/SecurityConfig.java
+++ lab/80-security-views/src/main/java/config/SecurityConfig.java
@@ -14,19 +14,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		//	TODO-04: Set the login page (below) so it uses '/login' and the access denied page so it uses '/denied'.
+		//	TODO-04: Set the login page with '/login' and the access denied page with '/denied'.
+		//  - Let the application restart.
 		//  - Click on the "View Account List" - http://localhost:8080/accounts/accountList
-		//	  You should now be taken to the login page
-		//	- Try to log in using an incorrect user/password such as 'foo', 'foo'.
+		//	  You should now be taken to the login page.
+		//	- Log in using an incorrect user/password such as 'foo', 'foo'.
 		//    Note that the you are sent back to the login page with a friendly message.
 		//	- Take a look at login.html to see if you understand how the page
 		//	  displays the error message in response to a bad username or password.
 		
-		//	TODO-05: Check the username/password of the user defined in configureGlobal (below)
-		//	and use it to log into the application.
-		//	However this user is NOT allowed to access the 'Account List' page yet. 
-		//	After log in, you SHOULD expect to see the access denied page - set this to
-		//  be '/denied' below. 
+		//	TODO-05: Log in with correct username and password, "viewer/viewer"
+		//	- Access 'Account List' page.
+		//	- You SHOULD expect to see the access denied page - set this to
+		//    be '/denied' below.
 
 		http
 			.formLogin()
@@ -37,13 +37,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.accessDeniedPage("/TODO-04b")
 				.and()
 				
-		//	TODO-06: As defined below, users with role EDITOR can already access '/accounts/account*'. 
-		//	- Update the configuration so users with role VIEWER can ALSO access that same URL pattern.
-		//	- After completing this task, let the web application redeploy.
-		//  - Login as user 'viewer' and you should now be able to access the
-		//	  account list and account details.
-		//	- Try to Edit the Account Details again verify you get an Access Denied error because
-		//	  'viewer' is not an EDITOR.
+		    //	TODO-06: As defined below, users with role EDITOR can already access '/accounts/account*'.
+		    //	- Update the configuration so users with role VIEWER can ALSO access that same URL pattern.
+		    //	- After completing this task, let the application restart.
+		    //  - Login as user 'viewer' and you should now be able to access the
+		    //	  account list and account details.
+		    //	- Try to Edit the Account Details again verify you get an Access Denied error because
+		    //	  'viewer' is not an EDITOR.
 				
 			.authorizeRequests()
 				.mvcMatchers("/accounts/resources/**").permitAll()
@@ -51,12 +51,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.mvcMatchers("/accounts/account*").hasRole("EDITOR")
 				.and()
 				
-		//	TODO-07: Log out by clicking on the 'log out' link. 
-		//	- Then try to access 'http://localhost:8080/accounts/hidden'.
-		//	  As you can see, this URL is currently not protected.
-		//	- Add an mvcMatchers with pattern /accounts/** to serve as a catch-all BELOW all other mvcMatchers calls.
-		//	  For this pattern, all users should be authenticated (no specific role required).
-		//	- Save and try to access this URL again, you should now be redirected to the login page.
+		    //	TODO-07: Log out by clicking on the 'log out' link.
+		    //	- Then try to access 'http://localhost:8080/accounts/hidden'.
+		    //	  As you can see, this URL is currently not protected.
+		    //	- Add an mvcMatchers with pattern /accounts/** to serve as a catch-all BELOW all other mvcMatchers calls.
+		    //	  For this pattern, all users should be authenticated (no specific role required).
+			// 	- Save and let the application restart.
+			//  - Access this URL again, you should now be redirected to the login page.
+
 				
 			.logout()
 				.permitAll()
