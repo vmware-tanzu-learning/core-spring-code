6f3a78a845a24a31f7440ae9be88ec6716fd8c4c
sashinpivotal <sashin@pivotal.io>
2020-01-15 13:57:24 -0500

    80-security-views - TODO-02 is clarified

--- lab/80-security-views/src/main/java/accounts/SecurityApplication.java
+++ lab/80-security-views/src/main/java/accounts/SecurityApplication.java
@@ -7,15 +7,19 @@ import config.MvcConfig;
 import config.RootConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 
 // TODO-01: Start this Spring Boot application
-
-// TODO-02: Access the home page at http://localhost:8080/ once it gets started
+// - Access the home page at http://localhost:8080/ once it gets started
 // - Click on the "View Account List" link, you should reach the list of accounts.
+//   You can access the above link because Spring Boot security is temporarily
+//   disabled.
 
-@SpringBootApplication
+// TODO-02: Enable Spring Boot security auto configuration
+// - Remove exclude = {SecurityAutoConfiguration.class} below
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // TODO-03: Import the SecurityConfig class - add it to the @Import below.
 // - Save all work and let the server restart.
 // - You should be able to access the home page, but should see a 404
