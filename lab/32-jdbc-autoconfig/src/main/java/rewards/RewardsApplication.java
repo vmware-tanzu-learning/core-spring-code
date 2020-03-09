be9ca1f6731571c5b26e155bda2c62b8df3a2c56
sashinpivotal <sashin@pivotal.io>
2020-03-09 15:45:10 -0400

    32-jdbc-autoconfig - cosmetic changes to TODO steps

--- lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
+++ lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
@@ -12,9 +12,9 @@ import org.springframework.boot.SpringApplication;
 //           by adding an annotation.
 
 // TODO-10 : Disable 'DataSource' auto-configuration
-//           - Use 'exclude' attribute of '@SpringBootApplication'
-//             excluding 'DataSourceAutoConfiguration' class
-//           - Import 'RewardsConfig' class. (Think about why.)
+// - Use 'exclude' attribute of '@SpringBootApplication'
+//   excluding 'DataSourceAutoConfiguration' class
+// - Import 'RewardsConfig' class. (Think about why.)
 
 // TODO-11 : Look in application.properties for the next step.
 
@@ -22,33 +22,31 @@ import org.springframework.boot.SpringApplication;
 //           The section titled "Build and Run using Command Line tools".
 
 public class RewardsApplication {
-	static final String SQL = "SELECT count(*) FROM T_ACCOUNT";
-	
+    static final String SQL = "SELECT count(*) FROM T_ACCOUNT";
+
     final Logger logger
             = LoggerFactory.getLogger(RewardsApplication.class);
-    
+
     public static void main(String[] args) {
-		SpringApplication.run(RewardsApplication.class, args);
-	}
+        SpringApplication.run(RewardsApplication.class, args);
+    }
 
     // TODO-05 : Move the SQL scripts from the
     //             `src/test/resources/rewards/testdb` to the
     //             `src/main/resources/`
-    //  Why are you doing this?  See detailed instructions if you do not know.
+    // - Why are you doing this?  See detailed instructions if you do not know.
 
-    // TODO-06 : Setup a command line runner that will query count from
+    // TODO-06 : Implement a command line runner that will query count from
     //           T_ACCOUNT table and log the count to the console
-    //           Use the SQL query and logger provided above.
-    //           Use the JdbcTemplate bean that Spring Boot creates for you
-    //           automatically.
+    // - Use the SQL query and logger provided above.
+    // - Use the JdbcTemplate bean that Spring Boot creates for you automatically.
     //
     // TODO-07   Enable full debugging in order to observe how Spring Boot
-	//           performs its auto-configuration logic
-	//           - Follow TO-DO-07 in application.properties
-    //             then come back here.
-    //           - Run the application
-    //           - In the console output, find "CONDITIONS EVALUATION REPORT"
-    //             Note that "DataSourceAutoConfiguration" is mentioned under
-	//             positive match.
+    //           performs its auto-configuration logic
+    // - Follow TO-DO-07 in application.properties, then come back here.
+    // - Run the application
+    // - In the console output, find "CONDITIONS EVALUATION REPORT".
+    // - Note that "DataSourceAutoConfiguration" is mentioned under
+    //   positive match.
 
 }
