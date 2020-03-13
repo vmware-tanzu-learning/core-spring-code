6307f0a7050a10d6caa8a47125713c4db0be97ce
sashinpivotal <sashin@pivotal.io>
2020-03-13 14:59:16 -0400

    62-reactive-spring - clean up comments

--- lab/62-reactive-spring-solution/src/main/java/accounts/web/AccountController.java
+++ lab/62-reactive-spring-solution/src/main/java/accounts/web/AccountController.java
@@ -1,7 +1,7 @@
 package accounts.web;
 
-import java.util.List;
-
+import accounts.AccountManager;
+import common.util.ThreadUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,26 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import accounts.AccountManager;
-import common.util.ThreadUtils;
 import rewards.internal.account.Account;
-import rewards.internal.account.Beneficiary;
 
-/**
- * A Spring MVC @Controller controller handling requests to fetch or view
- * Account information. This is a simplified version of the controller from
- * <code>rest-ws</code> since we only need to fetch accounts by id.
- * <p>
- * Note that all the Account application classes are imported from the
- * <tt>rewards-db</tt> project:
- * <ul>
- * <li>Domain objects: {@link Account} and {@link Beneficiary}</li>
- * <li>Service layer: {@link AccountManager} interface and its
- * implementations</li>
- * <li>No repository layer is being used - the account-manager does
- * everything</li>
- */
+import java.util.List;
+
 @Controller
 public class AccountController {
 
@@ -43,14 +27,7 @@ public class AccountController {
 	}
 
 	/**
-	 * <p>
 	 * Provide a model with an account for the account detail page.
-	 * </p>
-	 * 
-	 * @param id
-	 *            the id of the account
-	 * @param model
-	 *            the "implicit" model created by Spring MVC
 	 */
 	@RequestMapping("/accountDetails")
 	public String accountDetails(@RequestParam("entityId") long id, Model model) {
@@ -59,12 +36,7 @@ public class AccountController {
 	}
 
 	/**
-	 * <p>
 	 * Provide a model with a list of all accounts for the account List page.
-	 * </p>
-	 * 
-	 * @param model
-	 *            the "implicit" model created by Spring MVC
 	 */
 	@RequestMapping("/accountList")
 	public String accountList(Model model) {
@@ -73,12 +45,7 @@ public class AccountController {
 	}
 
 	/**
-	 * <p>
 	 * RESTful method to fetch an account.
-	 * </p>
-	 * 
-	 * @param id
-	 *            The account id from the URL
 	 */
 	@RequestMapping("/accounts")
 	public @ResponseBody List<Account> allAccounts() {
@@ -86,12 +53,7 @@ public class AccountController {
 	}
 
 	/**
-	 * <p>
 	 * RESTful method to fetch an account.
-	 * </p>
-	 * 
-	 * @param id
-	 *            The account id from the URL
 	 */
 	@RequestMapping("/accounts/{id}")
 	public @ResponseBody Account accounts(@PathVariable long id) {
