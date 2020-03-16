8d40a412be5b9e0e02ec0363358eba4c41c1dfc5
sashinpivotal <sashin@pivotal.io>
2020-03-16 09:19:26 -0400

    40-boot-test-solution - use proper indentation for the test code

--- lab/40-boot-test-solution/src/test/java/accounts/web/AccountControllerBootTests.java
+++ lab/40-boot-test-solution/src/test/java/accounts/web/AccountControllerBootTests.java
@@ -25,127 +25,144 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * These tests run the AccountController using the MockMVC framework.
  * The server does not need to be running.
  */
-@AutoConfigureDataJpa
 @WebMvcTest(AccountController.class)
+@AutoConfigureDataJpa
 public class AccountControllerBootTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@MockBean
-	private AccountManager accountManager;
+    @MockBean
+    private AccountManager accountManager;
 
-	@Test
-	public void accountDetails() throws Exception {
+    @Test
+    public void accountDetails() throws Exception {
 
-		// arrange
-		given(accountManager.getAccount(0L)).willReturn(new Account("1234567890", "John Doe"));
+        // arrange
+        given(accountManager.getAccount(0L))
+				.willReturn(new Account("1234567890", "John Doe"));
 
-		// act and assert
-		mockMvc.perform(get("/accounts/0")).andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("name").value("John Doe")).andExpect(jsonPath("number").value("1234567890"));
+        // act and assert
+        mockMvc.perform(get("/accounts/0"))
+               .andExpect(status().isOk())
+               .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+               .andExpect(jsonPath("name").value("John Doe"))
+               .andExpect(jsonPath("number").value("1234567890"));
 
-		// verify
-		verify(accountManager).getAccount(0L);
+        // verify
+        verify(accountManager).getAccount(0L);
 
-	}
+    }
 
-	@Test
-	public void accountDetailsFail() throws Exception {
+    @Test
+    public void accountDetailsFail() throws Exception {
 
-		given(accountManager.getAccount(any(Long.class)))
-				.willThrow(new IllegalArgumentException("No such account with id " + 0L));
+        given(accountManager.getAccount(any(Long.class)))
+                .willThrow(new IllegalArgumentException("No such account with id " + 0L));
 
-		mockMvc.perform(get("/accounts/0")).andExpect(status().isNotFound());
+        mockMvc.perform(get("/accounts/0"))
+               .andExpect(status().isNotFound());
 
-		verify(accountManager).getAccount(any(Long.class));
+        verify(accountManager).getAccount(any(Long.class));
 
-	}
+    }
 
-	@Test
-	public void accountSummary() throws Exception {
+    @Test
+    public void accountSummary() throws Exception {
 
-		List<Account> testAccounts = Arrays.asList(new Account("123456789", "John Doe"));
-		given(accountManager.getAllAccounts()).willReturn(testAccounts);
+        List<Account> testAccounts = Arrays.asList(new Account("123456789", "John Doe"));
+        given(accountManager.getAllAccounts())
+				.willReturn(testAccounts);
 
-		mockMvc.perform(get("/accounts")).andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$..number").value("123456789")).andExpect(jsonPath("$..name").value("John Doe"));
+        mockMvc.perform(get("/accounts"))
+               .andExpect(status().isOk())
+               .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+               .andExpect(jsonPath("$..number").value("123456789"))
+               .andExpect(jsonPath("$..name").value("John Doe"));
 
-		verify(accountManager).getAllAccounts();
+        verify(accountManager).getAllAccounts();
 
-	}
+    }
 
-	@Test
-	public void createAccount() throws Exception {
+    @Test
+    public void createAccount() throws Exception {
 
-		Account testAccount = new Account("1234512345", "Mary Jones");
-		testAccount.setEntityId(21L);
-		given(accountManager.save(any(Account.class))).willReturn(testAccount);
+        Account testAccount = new Account("1234512345", "Mary Jones");
+        testAccount.setEntityId(21L);
 
-		mockMvc.perform(post("/accounts").contentType(MediaType.APPLICATION_JSON)
-				.content(asJsonString(testAccount)).accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated())
-				.andExpect(header().string("Location", "http://localhost/accounts/21"));
+        given(accountManager.save(any(Account.class)))
+				.willReturn(testAccount);
 
-		verify(accountManager).save(any(Account.class));
+        mockMvc.perform(post("/accounts")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(asJsonString(testAccount))
+							.accept(MediaType.APPLICATION_JSON))
+               .andExpect(status().isCreated())
+               .andExpect(header().string("Location", "http://localhost/accounts/21"));
 
-	}
+        verify(accountManager).save(any(Account.class));
 
-	@Test
-	public void getBeneficiary() throws Exception {
+    }
 
-		Account account = new Account("1234567890", "John Doe");
-		account.addBeneficiary("Corgan", new Percentage(0.1));
-		given(accountManager.getAccount(0L)).willReturn(account);
+    @Test
+    public void getBeneficiary() throws Exception {
 
-		mockMvc.perform(get("/accounts/{accountId}/beneficiaries/{beneficiaryName}", 0L, "Corgan"))
-				.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("name").value("Corgan")).andExpect(jsonPath("allocationPercentage").value("0.1"));
+        Account account = new Account("1234567890", "John Doe");
+        account.addBeneficiary("Corgan", new Percentage(0.1));
 
-		verify(accountManager).getAccount(0L);
-	}
+        given(accountManager.getAccount(0L))
+				.willReturn(account);
 
-	@Test
-	public void addBeneficiary() throws Exception {
+        mockMvc.perform(get("/accounts/{accountId}/beneficiaries/{beneficiaryName}", 0L, "Corgan"))
+               .andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
+               .andExpect(jsonPath("name").value("Corgan"))
+               .andExpect(jsonPath("allocationPercentage").value("0.1"));
 
-		mockMvc.perform(post("/accounts/{entityId}/beneficiaries", 0L).content("Kate")).andExpect(status().isCreated())
-				.andExpect(header().string("Location", "http://localhost/accounts/0/beneficiaries/Kate"));
-	}
+        verify(accountManager).getAccount(0L);
+    }
 
-	@Test
-	public void removeBeneficiary() throws Exception {
+    @Test
+    public void addBeneficiary() throws Exception {
 
-		Account account = new Account("1234567890", "John Doe");
-		account.addBeneficiary("Corgan", new Percentage(0.1));
-		given(accountManager.getAccount(0L)).willReturn(account);
+        mockMvc.perform(post("/accounts/{entityId}/beneficiaries", 0L)
+				            .content("Kate"))
+               .andExpect(status().isCreated())
+               .andExpect(header().string("Location", "http://localhost/accounts/0/beneficiaries/Kate"));
+    }
 
-		mockMvc.perform(delete("/accounts/{entityId}/beneficiaries/{name}", 0L, "Corgan"))
-				.andExpect(status().isNoContent());
+    @Test
+    public void removeBeneficiary() throws Exception {
 
-		verify(accountManager).getAccount(0L);
+        Account account = new Account("1234567890", "John Doe");
+        account.addBeneficiary("Corgan", new Percentage(0.1));
+        given(accountManager.getAccount(0L)).willReturn(account);
 
-	}
+        mockMvc.perform(delete("/accounts/{entityId}/beneficiaries/{name}", 0L, "Corgan"))
+               .andExpect(status().isNoContent());
 
-	@Test
-	public void removeBeneficiaryFail() throws Exception {
-		Account account = new Account("1234567890", "John Doe");
-		given(accountManager.getAccount(0L)).willReturn(account);
+        verify(accountManager).getAccount(0L);
 
-		mockMvc.perform(delete("/accounts/{entityId}/beneficiaries/{name}", 0L, "Noname"))
-				.andExpect(status().isNotFound());
+    }
 
-		verify(accountManager).getAccount(0L);
-	}
+    @Test
+    public void removeBeneficiaryFail() throws Exception {
+        Account account = new Account("1234567890", "John Doe");
+        given(accountManager.getAccount(0L)).willReturn(account);
 
-	protected static String asJsonString(final Object obj) {
-		try {
-			final ObjectMapper mapper = new ObjectMapper();
-			final String jsonContent = mapper.writeValueAsString(obj);
-			return jsonContent;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+        mockMvc.perform(delete("/accounts/{entityId}/beneficiaries/{name}", 0L, "Noname"))
+               .andExpect(status().isNotFound());
+
+        verify(accountManager).getAccount(0L);
+    }
+
+    protected static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
