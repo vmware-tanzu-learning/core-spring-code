2724bb2b9e8fbbb8dfca990daf0cdb4724118700
sashinpivotal <sashin@pivotal.io>
2020-05-18 15:44:38 -0400

    42-security-rest - TODO 15 and 19 use /accounts/0

--- lab/42-security-rest/src/main/java/accounts/RestWsApplication.java
+++ lab/42-security-rest/src/main/java/accounts/RestWsApplication.java
@@ -65,24 +65,24 @@ public class RestWsApplication {
 // TODO-15 (Optional): Verify that the newly added custom UserDetailsService works
 // - Re-run this application
 // - Using Chrome Incognito browser, access
-//   http://localhost:8080/accounts
+//   http://localhost:8080/accounts/0
 // - Enter "mary"/"mary" and verify accounts data gets displayed
 // - If you want to use "curl", use
-//   curl -i -u mary:mary http://localhost:8080/accounts
+//   curl -i -u mary:mary http://localhost:8080/accounts/0
 //
 // - Close the Chrome Incognito browser and start a new one
 // - Using Chrome Incognito browser, access
-//   http://localhost:8080/accounts
+//   http://localhost:8080/accounts/0
 // - Enter "joe"/"joe" and verify accounts data gets displayed
 // - If you want to use "curl", use
-//   curl -i -u joe:joe http://localhost:8080/accounts
+//   curl -i -u joe:joe http://localhost:8080/accounts/0
 
 // ------------------------------------------------
 
 // TODO-19 (Optional): Verify that the newly added custom AuthenticationProvider works
 // - Re-run this application
 // - Using Chrome Incognito browser, access
-//   http://localhost:8080/accounts
+//   http://localhost:8080/accounts/0
 // - Enter "spring"/"spring" and verify accounts data
 // - If you want to use "curl", use
-//   curl -i -u spring:spring http://localhost:8080/accounts
+//   curl -i -u spring:spring http://localhost:8080/accounts/0
