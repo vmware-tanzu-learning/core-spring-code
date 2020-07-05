4803e10f158b8231079c08815bb50a8e1fcf4682
sashinpivotal <sashin@pivotal.io>
2020-07-05 17:04:08 -0400

    44-actuator-solution - grouping health indicators

---
+++ lab/44-actuator-solution/src/main/java/accounts/web/RestaurantCustomEndpoint.java
@@ -0,0 +1,43 @@
+package accounts.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.endpoint.annotation.DeleteOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
+import org.springframework.stereotype.Component;
+import rewards.internal.restaurant.RestaurantRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Endpoint(id = "restaurant")
+public class RestaurantEndpoint {
+
+    Map<String, String> map = new HashMap<>();
+
+    public RestaurantEndpoint(RestaurantRepository restaurantRepository,
+                              @Value("${info.restaurant.location}") String location) {
+        Long restaurantCount = restaurantRepository.getRestaurantCount();
+        map.put("restaurant.count", restaurantCount.toString());
+        map.put("restaurant.location", location);
+    }
+
+    @ReadOperation
+    public Map<String, String> readOperation() {
+        return map;
+    }
+
+    @WriteOperation
+    public Map<String, String> writeOperation(String key, String value) {
+        map.put(key, value);
+        return map;
+    }
+
+    @DeleteOperation
+    public Map<String, String> deleteOperation() {
+        map.clear();
+        return map;
+    }
+}
