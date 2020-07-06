bfd7858bc04a50e19a8166a785cded995bc78f0a
sashinpivotal <sashin@pivotal.io>
2020-07-06 10:06:07 -0400

    44-actuator-solution - added AccountClientCustomEndpointTests.java

---
+++ lab/44-actuator-solution/src/test/java/accounts/client/AccountClientCustomEndpointTests.java
@@ -0,0 +1,31 @@
+package accounts.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.wavefront.WavefrontMetricsExportAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration(exclude = {WavefrontMetricsExportAutoConfiguration.class})
+public class AccountClientCustomEndpointTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void restaurant_custom_endpoint_returns_valid_data() {
+        ResponseEntity<String> responseEntity
+                = restTemplate.withBasicAuth("admin", "admin")
+                              .getForEntity("/actuator/restaurant", String.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).contains("restaurant.location").contains("New York");
+    }
+
+}
