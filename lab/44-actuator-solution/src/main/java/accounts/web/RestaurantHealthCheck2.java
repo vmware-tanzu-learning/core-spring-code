b88c43afa9e63532997f9cfd274b03e8759a04c7
sashinpivotal <sashin@pivotal.io>
2020-10-13 18:15:35 -0400

    44-actuator-solution - includes RestaurantHealthCheck2 that extends AbstractHealthIndicator

---
+++ lab/44-actuator-solution/src/main/java/accounts/web/RestaurantHealthCheck2.java
@@ -0,0 +1,29 @@
+package accounts.web;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+import rewards.internal.restaurant.RestaurantRepository;
+
+// This is a HealthIndicator example that uses the AbstractHealthIndicator
+@Component
+public class RestaurantHealthCheck2 extends AbstractHealthIndicator {
+    private final RestaurantRepository restaurantRepository;
+
+    public RestaurantHealthCheck2(RestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+
+        Long restaurantCount = restaurantRepository.getRestaurantCount();
+        if (restaurantCount > 0) {
+            builder.up()
+                   .withDetail("restaurantCount", restaurantCount);
+        } else {
+            builder.status(new Status("NO_RESTAURANTS"));
+        }
+    }
+}
