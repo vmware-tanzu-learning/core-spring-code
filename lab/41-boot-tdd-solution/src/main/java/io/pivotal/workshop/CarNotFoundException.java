95893de6054c917af5a6432a3378a347f88fc483
Bill Kable <bkable@gmail.com>
2022-03-08 16:29:27 -0600

    [course #24, code #12] removed optional labs
    Signed-off-by: Joe Moore <joe.moore@gmail.com>

--- lab/41-boot-tdd-solution/src/main/java/io/pivotal/workshop/CarNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package io.pivotal.workshop;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class CarNotFoundException extends RuntimeException {
-}
--- lab/41-boot-tdd-solution/src/main/java/io/pivotal/workshop/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package io.pivotal.workshop;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-    Car findByName(String name);
-}
