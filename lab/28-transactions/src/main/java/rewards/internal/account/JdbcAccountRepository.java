8c27e90cd5c4a250cc282984dd13be806c28c707
Sergi Almar <sergi.almar@gmail.com>
2022-11-03 16:43:40 +0100

    Fix deprecations in 28-transactions

--- lab/28-transactions/src/main/java/rewards/internal/account/JdbcAccountRepository.java
+++ lab/28-transactions/src/main/java/rewards/internal/account/JdbcAccountRepository.java
@@ -31,7 +31,7 @@ public class JdbcAccountRepository implements AccountRepository {
 	
 	public Account findByCreditCard(String creditCardNumber) {
 		String sql = "select a.ID as ID, a.NUMBER as ACCOUNT_NUMBER, a.NAME as ACCOUNT_NAME, c.NUMBER as CREDIT_CARD_NUMBER, b.NAME as BENEFICIARY_NAME, b.ALLOCATION_PERCENTAGE as BENEFICIARY_ALLOCATION_PERCENTAGE, b.SAVINGS as BENEFICIARY_SAVINGS from T_ACCOUNT a, T_ACCOUNT_BENEFICIARY b, T_ACCOUNT_CREDIT_CARD c where ID = b.ACCOUNT_ID and ID = c.ACCOUNT_ID and c.NUMBER = ?";
-		return jdbcTemplate.query(sql, new Object[] { creditCardNumber }, accountExtractor);
+		return jdbcTemplate.query(sql, accountExtractor, creditCardNumber);
 	}
 
 	public void updateBeneficiaries(Account account) {
