714758ddce03f7ee178d6bc7cf6555a228076666
Felipe Gutierrez <felipeg@gmail.com>
2022-11-03 11:18:16 -0400

    Fixed the Client test failind in lesson 40 - solution

--- lab/40-boot-test-solution/src/main/java/accounts/BootTestSolutionApplication.java
+++ lab/40-boot-test-solution/src/main/java/accounts/BootTestSolutionApplication.java
@@ -3,6 +3,7 @@ package accounts;
 import config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Import;
  */
 @SpringBootApplication
 @Import(AppConfig.class)
+@EntityScan("rewards.internal")
 public class BootTestSolutionApplication {
 
 	public static void main(String[] args) {
